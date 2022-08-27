4e1a738940eccef6e7d04187ffed4efcee70746a
Muh.Taufiqulsadi <mtaufiqulsadi@gmail.com>
2022-08-27 16:13:05 +0800

    feat(driver-management): show username on  driver card component

--- components/DriverCard/DriverCard.component.styles.tsx
+++ components/DriverCard/DriverCard.component.styles.tsx
@@ -1,13 +1,32 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  padding: 1rem;
   background-color: white;
   border-radius: 0.125rem;
 `
 
-export const Title = styled.p`
+export const GrayText = styled.p`
   color: #c6c6c6;
+`
+
+export const PrimaryText = styled.p`
+  color: #ff453b;
+`
+
+export const DriverIDTitle = styled(GrayText)`
+  display: inline-block;
+  margin-right: 0.5rem;
+`
+export const DriverIDValue = styled(PrimaryText)`
+  display: inline-block;
+`
+
+export const DriverIDContainer = styled.div`
+  padding: 1rem;
+  border-bottom: 0.5px solid gray;
+`
+
+export const Title = styled(GrayText)`
   font-size: 0.75rem;
   margin: 0;
 `
@@ -17,10 +36,11 @@ export const Description = styled.p`
 `
 
 export const ContentContainer = styled.div`
-  padding: 0.5rem 0;
+  padding: 0.5rem 1rem;
 `
 
 export const Avatar = styled.img`
   border-radius: 50%;
-  padding: 0.5rem 0;
+  padding: 1rem;
 `
+
--- components/DriverCard/DriverCard.component.tsx
+++ components/DriverCard/DriverCard.component.tsx
@@ -1,16 +1,18 @@
 import { Component } from "../../index.types";
-import { Avatar, Container, ContentContainer, Description, Title } from "./DriverCard.component.styles";
+import { Avatar, Container, ContentContainer, Description, DriverIDContainer, DriverIDTitle, DriverIDValue, GrayText, PrimaryText, Title } from "./DriverCard.component.styles";
 
 import { Props } from "./DriverCard.component.types";
 
 const DriverCard:Component<Props> = ({user})=>{
   return(
   <Container>
-    <ContentContainer>
-      <Avatar
-        src={user.profilePicture}
-      />
-    </ContentContainer>
+    <DriverIDContainer>
+      <DriverIDTitle>Driver ID</DriverIDTitle>
+      <DriverIDValue>{user.username.toUpperCase()}</DriverIDValue>
+    </DriverIDContainer>
+    <Avatar
+      src={user.profilePicture}
+    />
     <ContentContainer>
       <Title>Nama Driver</Title>
       <Description>{user.firstName}, {user.lastName}</Description>
