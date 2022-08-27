f1fa2740c929194a83df601f64bbd96fdfc6b8d6
Muh.Taufiqulsadi <mtaufiqulsadi@gmail.com>
2022-08-27 14:21:45 +0800

    feat(driver-management): revamp driver card component

---
+++ components/DriverCard/DriverCard.component.styles.tsx
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  padding: 1rem;
+  background-color: white;
+`
+
+export const Title = styled.p`
+  color: #c6c6c6;
+  font-size: 12px;
+  margin: 0;
+`
+
+export const Description = styled.p`
+  margin: 0;
+`
+
+export const ContentContainer = styled.div`
+  padding: 0.5rem 0;
+`
--- components/DriverCard/DriverCard.component.tsx
+++ components/DriverCard/DriverCard.component.tsx
@@ -1,15 +1,28 @@
 import { Component } from "../../index.types";
+import { Container, ContentContainer, Description, Title } from "./DriverCard.component.styles";
 
 import { Props } from "./DriverCard.component.types";
 
 const DriverCard:Component<Props> = ({user})=>{
   return(
-  <div>
-    <p>{user.firstName}, {user.lastName}</p>
-    <p>{user.phone}</p>
-    <p>{user.email}</p>
-    <p>{user.dob}</p>
-  </div>)
+  <Container>
+    <ContentContainer>
+      <Title>Nama Driver</Title>
+      <Description>{user.firstName}, {user.lastName}</Description>
+    </ContentContainer>
+    <ContentContainer>
+      <Title>Telepon</Title>
+      <Description>{user.phone}</Description>
+    </ContentContainer>
+    <ContentContainer>
+      <Title>Email</Title>
+      <Description>{user.email}</Description>
+    </ContentContainer>
+    <ContentContainer>
+      <Title>Tanggal Lahir</Title>
+      <Description>{user.dob}</Description>
+    </ContentContainer>
+  </Container>)
 }
 
 export default DriverCard;
