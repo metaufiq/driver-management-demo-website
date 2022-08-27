6a58c0354a05532b66cc3aa8c31fbfb0bfa04f9d
Muh.Taufiqulsadi <mtaufiqulsadi@gmail.com>
2022-08-27 15:21:27 +0800

    feat(driver-management): revamp driver management header

--- pages/driver-management/DriverManagement.component.styles.tsx
+++ pages/driver-management/DriverManagement.component.styles.tsx
@@ -2,6 +2,24 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
   background-color: #fafafa;
+  padding: 2rem;
+`
+
+export const HeaderContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  background-color: white;
+  padding: 1rem;
+`
+
+export const HeaderTitle = styled.h1`
+  color: #ff453b;
+  margin: 0.125rem 0;
+`
+
+export const HeaderDescription = styled.p`
+  color: gray;
+  margin: 0;
 `
 
 export const ListUserContainer = styled.div`
@@ -9,12 +27,11 @@ export const ListUserContainer = styled.div`
   display: flex;
   column-gap: 1rem;
   background-color: #fafafa;
-  padding: 2rem;
+  padding: 2rem 0;
 `
 
 export const PaginationContainer = styled.div`
   display: flex;
   column-gap: 1rem;
-  padding: 2rem;
   justify-content: center;
 `
