dec64dcf75ffeec5580ce2320cbb69275dc3dfa2
Muh.Taufiqulsadi <mtaufiqulsadi@gmail.com>
2022-08-28 11:48:21 +0800

    feat(driver-management): add new constants to handle sidebar menu

--- constants/index.ts
+++
@@ -1,4 +0,0 @@
-export const TOTAL_USER = 30;
-export const USER_PER_PAGE = 5;
-export const PAGE = TOTAL_USER/USER_PER_PAGE;
-export const USER_AVATAR_URL = 'https://randomuser.me/api/portraits/med/men/18.jpg';
---
+++ constants/index.tsx
@@ -0,0 +1,26 @@
+import {TiHome} from 'react-icons/ti'
+import {RiCalendar2Fill} from 'react-icons/ri'
+import {FaUserCircle} from 'react-icons/fa'
+
+export const TOTAL_USER = 30;
+export const USER_PER_PAGE = 5;
+export const PAGE = TOTAL_USER/USER_PER_PAGE;
+export const USER_AVATAR_URL = 'https://randomuser.me/api/portraits/med/men/18.jpg';
+export const SIDEBAR_MENU = [
+  {
+      title: 'Beranda',
+      path: '#',
+      icon: <TiHome />,
+  },
+  {
+      title: 'Driver Management',
+      path: '/driver-management',
+      icon: <FaUserCircle />,
+      isRoot: true,
+  },
+  {
+      title: 'Pickup',
+      path: '#',
+      icon: <RiCalendar2Fill />
+  },
+]
