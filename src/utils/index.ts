cc48d91b46697393f89f72a9a7ba75a472226016
Muh.Taufiqulsadi <mtaufiqulsadi@gmail.com>
2022-08-29 15:28:56 +0800

    chore(driver-management): add jsdoc for utils

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,7 +1,17 @@
-export const _padTo2Digits = (num:number) => {
+/**
+ * formator number to two digits number with string type
+ * @param {number} num number
+ * @returns {string} two digits number with string type
+ */
+export const _padTo2Digits = (num:number): string => {
   return num.toString().padStart(2, '0');
 }
 
+/**
+ * format ISO date to format DD-MM-YYYY
+ * @param {string} ISODate ISO date
+ * @returns {string} date with format DD-MM-YYYY
+ */
 export const formatISODateToDate = (ISODate: string) =>{
   const date = new Date(ISODate);
   
