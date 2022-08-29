7099652da226e4d3a2644b9d8967905a0fc10631
Muh.Taufiqulsadi <mtaufiqulsadi@gmail.com>
2022-08-29 23:21:36 +0800

    chore(refactor-project): fix issue bundle size

--- next.config.js
+++ next.config.js
@@ -1,8 +1,22 @@
 /** @type {import('next').NextConfig} */
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+})
+
+const path = require('path');
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   pageExtensions: ['page.tsx', 'page.ts', 'page.jsx', 'page.js'],
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    config.resolve.alias['react-icons'] = path.resolve(
+      __dirname,
+      'node_modules',
+      'react-icons'
+    )
+    return config
+  },
 }
 
-module.exports = nextConfig
+module.exports = withBundleAnalyzer(nextConfig)
